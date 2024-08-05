be5692fbb75ca41503f69c5dc86538fb7425ff2e
Tadedp <tadeodp22@gmail.com>
2024-08-05 02:23:33 -0300

    Added coordinator and combinator process and shared memory. Now it's funcionalgit add pyfilters.py

--- pyfilters.py
+++ pyfilters.py
@@ -1,46 +1,64 @@
 import argparse
 import multiprocessing
-import matplotlib.pyplot as plt
 from PIL import Image
 from scipy import ndimage
 
 class InvalidValueException(Exception):
     pass
 
-def split_image(image, processes):
+def split_image(image, num_parts, width, height):
     image_parts = []
-    width = int(image.size[0] / processes)
-    height = int(image.size[1])    
-    
-    for i in range(processes):
-        area = (i*width, 0, (i*width) + width, height)
-        #print(area)
-        #print("Tamaño: " + str(image.size))
+    part_height = height // num_parts
+    for i in range(num_parts):
+        area = (0, i * part_height, width, (i * part_height) + part_height)
         image_part = image.crop(area)
         image_parts.append(image_part)
-    
+        
     return image_parts
 
-def apply_filter(image_part, filter_num):
+def apply_filter(image_part, shared_array, start, end, filter_num, worker_pipe):
     if filter_num == 1:
-        return ndimage.gaussian_filter(image_part, sigma=3)
-    elif filter_num == 2:
-        return ndimage.median_filter(image_part, size=3)
-    elif filter_num == 3:
-        return ndimage.sobel(image_part)
-    elif filter_num == 4:
-        return ndimage.laplace(image_part)
-    elif filter_num == 5:
-        return ndimage.prewitt(image_part)
+        shared_array[start:end] = ndimage.gaussian_filter(image_part, sigma=3).tobytes()
     
-    plt.imshow(image_part)
-    plt.axis("off")
-    plt.show()    
+    elif filter_num == 2:
+        shared_array[start:end] = ndimage.median_filter(image_part, size=3).tobytes()
+    
+    elif filter_num == 3:
+        shared_array[start:end] = ndimage.sobel(image_part).tobytes()
+    
+    elif filter_num == 4:
+        shared_array[start:end] = ndimage.laplace(image_part).tobytes()
+    
+    elif filter_num == 5:
+        shared_array[start:end] = ndimage.prewitt(image_part).tobytes()
+    
+    worker_pipe.send("Filter applied.")
+    worker_pipe.close()
+    
+def coordinate_combine(pipes, shared_array, width, height, workers):
+    for coordinator_pipe in pipes:
+        coordinator_pipe.recv()
+        coordinator_pipe.close()
+
+    current_height = 0
+    result_image = Image.new("RGB", (width, height)) 
+    part_size_bytes = width * (height // len(workers)) * 3    
+    for i in range(len(workers)):
+        start = i * part_size_bytes
+        end = start + part_size_bytes
+        image_part = Image.frombytes("RGB", (width, height // len(workers)), bytes(shared_array[start:end]))   
+        result_image.paste(image_part, (0, current_height))
+        current_height += (height // len(workers))
+        
+    result_image.show()
+    result_image.save("image.png")
     
 def main(path, processes):
     image = Image.open(path)
-    image_parts = split_image(image, processes)
-    
+    width = image.size[0]
+    height = image.size[1]    
+    image_parts = split_image(image, processes - 1, width, height)
+        
     print("FILTERS")
     print("1- Gaussian filter.")
     print("2- Median filter.")
@@ -57,16 +75,31 @@ def main(path, processes):
                 break
         except Exception:
             print("Error: Invalid value. Enter an integer between 1 and 5.")    
-            
-    workers = []    
+    
+    pipes = []    
+    workers = []
+    total_size_bytes = width * height * 3
+    shared_array = multiprocessing.Array("B", total_size_bytes)
+    part_size_bytes = width * (height // (processes - 1)) * 3
+    
     for i in range(processes):
-        process = multiprocessing.Process(target=apply_filter, args=(image_parts[i], filter_num))    
-        workers.append(process)
-        process.start()
+        if i < processes - 1:
+            coordinator_pipe, worker_pipe = multiprocessing.Pipe()
+            start = i * part_size_bytes
+            end = start + part_size_bytes
+            process = multiprocessing.Process(target=apply_filter, args=(image_parts[i], shared_array, start, end, filter_num, worker_pipe))
+            workers.append(process)
+            pipes.append(coordinator_pipe)
     
+        else:    
+            coordinator = multiprocessing.Process(target=coordinate_combine, args=(pipes, shared_array, width, height, workers))    
+            coordinator.start()
+        
     for process in workers:
+        process.start()
         process.join()
-    
+    coordinator.join()
+
     image.close()
 
 if __name__ == '__main__':
@@ -74,4 +107,10 @@ if __name__ == '__main__':
     parser.add_argument("path", type=str, help="Path of an image.")
     parser.add_argument("--processes", type=int, help="Number of processes.", default=multiprocessing.cpu_count())
     args = parser.parse_args()
-    main(args.path, args.processes)
+    if args.processes < 2:
+        print("Error: Invalid value. Processes cannot be less than 2.")
+    else:
+        if args.path:
+            main(args.path, args.processes)
+        else:
+            print("Error: Invalid path.")
